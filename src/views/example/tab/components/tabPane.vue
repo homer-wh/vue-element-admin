909cbc291faf427480b4e99495b56a25dda7695b
Homer <homer@Homers-MBP.local>
2017-07-25 22:10:19 +0800

    首页 表格

--- src/views/example/tab/components/tabPane.vue
+++ src/views/example/tab/components/tabPane.vue
@@ -1,48 +1,48 @@
 <template>
   <el-table :data="list" border fit highlight-current-row style="width: 100%">
 
-    <el-table-column align="center" label="序号" width="65">
+    <!-- <el-table-column align="center" label="序号" width="65">
       <template scope="scope">
         <span>{{scope.row.id}}</span>
       </template>
-    </el-table-column>
+    </el-table-column> -->
 
-    <el-table-column width="180px" align="center" label="时间">
+    <el-table-column width="200px" align="center" label="时间">
       <template scope="scope">
         <span>{{scope.row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}')}}</span>
       </template>
     </el-table-column>
 
-    <el-table-column min-width="300px" label="标题">
+    <el-table-column min-width="600px" label="标题">
       <template scope="scope">
         <span class="link-type" @click="handleUpdate(scope.row)">{{scope.row.title}}</span>
         <el-tag>{{scope.row.type}}</el-tag>
       </template>
     </el-table-column>
 
-    <el-table-column width="110px" align="center" label="作者">
+    <!-- <el-table-column width="110px" align="center" label="作者">
       <template scope="scope">
         <span>{{scope.row.author}}</span>
       </template>
-    </el-table-column>
+    </el-table-column> -->
 
-    <el-table-column width="80px" label="重要性">
+    <el-table-column width="180px" label="重要性">
       <template scope="scope">
         <icon-svg v-for="n in +scope.row.importance" icon-class="wujiaoxing" class="meta-item__icon" :key="n"></icon-svg>
       </template>
     </el-table-column>
 
-    <el-table-column align="center" label="阅读数" width="95">
+   <!--  <el-table-column align="center" label="阅读数" width="95">
       <template scope="scope">
         <span>{{scope.row.pageviews}}</span>
       </template>
-    </el-table-column>
+    </el-table-column> -->
 
-    <el-table-column class-name="status-col" label="状态" width="90">
+    <!-- <el-table-column class-name="status-col" label="状态" width="90">
       <template scope="scope">
         <el-tag :type="scope.row.status | statusFilter">{{scope.row.status}}</el-tag>
       </template>
-    </el-table-column>
+    </el-table-column> -->
 
   </el-table>
 </template>
